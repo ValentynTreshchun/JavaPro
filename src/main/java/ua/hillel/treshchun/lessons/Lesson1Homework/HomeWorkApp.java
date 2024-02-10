dcba51ad84a4333209e07961e8d48fd0f53ea71d
Valentyn Treshchun <webberrbn@gmail.com>
2024-02-10 17:17:42 +0200

    HW1 reworked based on feedback and added in separate branch

--- src/main/java/ua/hillel/treshchun/lessons/Lesson1Homework/HomeWorkApp.java
+++ src/main/java/ua/hillel/treshchun/lessons/Lesson1Homework/HomeWorkApp.java
@@ -1,100 +1,81 @@
 package ua.hillel.treshchun.lessons.Lesson1Homework;
 
+import com.sun.tools.javac.Main;
+
+import java.sql.SQLOutput;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
         int year = 2200;
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-        System.out.println(isNumberWithin10_20(10, 10));
-        isPositiveOrZeroPrint(-18);
-        System.out.println(isNegative(0));
-        printStringNTimes("I'll have a public static final Borsch borsch new Borsch, please.", 3);
-        System.out.println("Year " + year + " is a leap year? " + isLeapYear(year));
+        HomeWorkApp HW1 = new HomeWorkApp();
+        HW1.printThreeWords();
+        HW1.checkSumSign();
+        HW1.printColor();
+        HW1.compareNumbers();
+        System.out.println(HW1.isNumberWithin10_20(10, 10));
+        HW1.isPositiveOrZeroPrint(-18);
+        System.out.println(HW1.isNegative(0));
+        HW1.printStringNTimes("I'll have a public static final Borsch borsch new Borsch, please.", 3);
+        System.out.println("Is " + year + " a leap year? " + HW1.isLeapYear(year));
 
     }
 
-//    task 2
-    public static void printThreeWords() {
+    private void printThreeWords() {
         System.out.println("Orange\n\nBanana\n\nApple");
     }
 
-//    task 3
-    public static void checkSumSign() {
-        int a = 5;
+    private void checkSumSign() {
+        int a = 9;
         int b = -6;
-        if ((a + b) >= 0) {
-            System.out.println("The sum is positive");
-        } else {
-            System.out.println("Sum is negative");
-        }
+        System.out.println((a + b) > 0 ? "A + B is positive" : "A + B is negative");
     }
 
-//    task 4
-    public static void printColor() {
-        int a = 101;
-        if (a <= 0) {
+    private void printColor() {
+        int a = 199;
+        if (a > 0) {
+            if (a <= 100) {
+                System.out.println("Yellow");
+            } else {
+                System.out.println("Green");
+            }
+        } else {
             System.out.println("Red");
-        } else if (0 < a && a <= 100) {
-            System.out.println("Yellow");
-        } else {
-            System.out.println("Green");
         }
     }
 
-//    task 5
-    public static void compareNumbers() {
+    private void compareNumbers() {
         int a = 19;
         int b = 27;
-        if (a >= b) {
-            System.out.println("A >= B");
-        } else {
-            System.out.println("A < B");
-        }
+        System.out.println((a >= b) ? "A >= B" : "A < B");
     }
 
-//    task 6
-    public static boolean isNumberWithin10_20(int a, int b) {
+    private boolean isNumberWithin10_20(int a, int b) {
         return (a + b) >= 10 && (a + b) <= 20;
     }
 
-//    task 7
-    public static void isPositiveOrZeroPrint(int a) {
-        if (a >= 0) {
-            System.out.println("The number is positive, or equals 0");
-        } else {
-            System.out.println("The number is negative");
-        }
+    private void isPositiveOrZeroPrint(int a) {
+        System.out.print("The number is ");
+        System.out.println((a >= 0) ? "positive or equals zero" : "negative");
     }
 
-//    task 8
-    public static boolean isNegative(int a) {
+    private boolean isNegative(int a) {
 //      in this method 0 is considered a positive number, same as in task 7
 //      since boolean can have only two states, 0 has to be included in either positive or negative
         return a < 0;
     }
 
-//    task 9
-    public static void printStringNTimes (String str, int n) {
+    private void printStringNTimes (String str, int n) {
         for (int i = 0; i < n; i++) {
             System.out.println(str);
         }
+        System.out.println("You idiot, borsch should be private, otherwise everyone can have it.");
     }
 
-//    task 10
-    public static boolean isLeapYear(int year) {
+    private boolean isLeapYear(int year) {
         if (year % 4 == 0) {
-            if (year % 100 == 0 && year % 400 != 0) {
-                return false;
-            }
-            else {
-                return true;
-            }
+            return year % 100 != 0 || year % 400 == 0;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 
 }
