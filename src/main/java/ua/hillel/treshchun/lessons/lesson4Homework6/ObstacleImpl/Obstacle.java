ebfebeb21184be63e6845ee4ccfe7c5bfd91e093
Valentyn Treshchun <webberrbn@gmail.com>
2024-02-27 21:10:39 +0200

    changed Obstacle constructor to protected to avoid ambiguity of obstacle type in HW6.java

--- src/main/java/ua/hillel/treshchun/lessons/lesson4Homework6/ObstacleImpl/Obstacle.java
+++ src/main/java/ua/hillel/treshchun/lessons/lesson4Homework6/ObstacleImpl/Obstacle.java
@@ -3,6 +3,10 @@ package ua.hillel.treshchun.lessons.lesson4Homework6.ObstacleImpl;
 public class Obstacle {
     protected int obstacleLength;
 
+    protected Obstacle() {
+        obstacleLength = -1;
+    }
+
     public int getObstacleLength() {
         return obstacleLength;
     }
